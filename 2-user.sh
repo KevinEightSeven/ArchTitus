77feadb5f8812191669dbc434aaaa98565d7f227
Kevin Walters <3036346+KevinEightSeven@users.noreply.github.com>
2021-12-05 12:26:34 -0500

    First commit

---
+++ 2-user.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+#-------------------------------------------------------------------------
+#   █████╗ ██████╗  ██████╗██╗  ██╗████████╗██╗████████╗██╗   ██╗███████╗
+#  ██╔══██╗██╔══██╗██╔════╝██║  ██║╚══██╔══╝██║╚══██╔══╝██║   ██║██╔════╝
+#  ███████║██████╔╝██║     ███████║   ██║   ██║   ██║   ██║   ██║███████╗
+#  ██╔══██║██╔══██╗██║     ██╔══██║   ██║   ██║   ██║   ██║   ██║╚════██║
+#  ██║  ██║██║  ██║╚██████╗██║  ██║   ██║   ██║   ██║   ╚██████╔╝███████║
+#  ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝   ╚═╝   ╚═╝   ╚═╝    ╚═════╝ ╚══════╝
+#-------------------------------------------------------------------------
+
+echo -e "\nINSTALLING AUR SOFTWARE\n"
+# You can solve users running this script as root with this and then doing the same for the next for statement. However I will leave this up to you.
+
+echo "CLONING: YAY"
+cd ~
+git clone "https://aur.archlinux.org/yay.git"
+cd ${HOME}/yay
+makepkg -si --noconfirm
+cd ~
+touch "$HOME/.cache/zshhistory"
+git clone "https://github.com/ChrisTitusTech/zsh"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/powerlevel10k
+ln -s "$HOME/zsh/.zshrc" $HOME/.zshrc
+
+PKGS=(
+'autojump'
+'dxvk-bin' # DXVK DirectX to Vulcan
+'google-chrome'
+'mangohud' # Gaming FPS Counter
+'mangohud-common'
+'plasma-pa'
+'ocs-url' # install packages from websites
+'snapper-gui-git'
+'ttf-droid'
+'ttf-hack'
+'ttf-meslo' # Nerdfont package
+'ttf-roboto'
+'snap-pac'
+)
+
+for PKG in "${PKGS[@]}"; do
+    yay -S --noconfirm $PKG
+done
+
+export PATH=$PATH:~/.local/bin
+cp -r $HOME/ArchTitus/dotfiles/* $HOME/.config/
+pip install konsave
+konsave -i $HOME/ArchTitus/kde.knsv
+sleep 1
+konsave -a kde
+
+echo -e "\nDone!\n"
+exit
